d3cad391b85f42cf5afe2832351f82bf25de956a
Brian Schulte <bschulte@Brians-MacBook-Pro.local>
2019-04-18 16:12:46 -0400

    export app as function rather than object

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
+
 import dotenv from "dotenv";
 dotenv.config();
+
 import path from "path";
 import Koa, { Context } from "koa";
 import bodyParser from "koa-bodyparser";
@@ -11,108 +13,108 @@ import als from "async-local-storage";
 import { v4 as uuid } from "uuid";
 import { ApolloServer } from "apollo-server-koa";
 import { Container } from "typedi";
-
-import {
-  buildSchemaSync,
-  useContainer as graphqlUseContainer,
-  buildSchema
-} from "type-graphql";
-
-import User from "./modules/user/user.entity";
-import { authChecker } from "./security/auth-checker";
-import { Logger } from "./logging/Logger";
-import * as userService from "./modules/user/user.service";
-import { isDevEnv } from "./common/helpers/util";
 import {
   useContainer as routingUseContainer,
   useKoaServer
 } from "routing-controllers";
+import {
+  buildSchemaSync,
+  useContainer as graphqlUseContainer
+} from "type-graphql";
 import { useContainer as ormUseContainer } from "typeorm";
+
+import User from "./modules/user/user.entity";
+import { authChecker } from "./security/auth-checker";
+import { Logger } from "./logging/Logger";
+import { isDevEnv } from "./common/helpers/util";
+import { UserService } from "./modules/user/user.service";
 
 const { APP_KEY = "super secret", PORT = 5555 } = process.env;
 
 const GRAPHQL_PATH = "/graphql";
 
-const _logger = new Logger("App.ts");
+export default async function createApp() {
+  const _logger = new Logger("App.ts");
+  const userService = Container.get(UserService);
 
-// Use DI for resolvers
-graphqlUseContainer(Container);
-const schema = buildSchemaSync({
-  resolvers: [`${__dirname}/**/*.resolver.ts`],
-  authChecker,
-  dateScalarMode: "timestamp"
-});
+  // Use DI for resolvers
+  graphqlUseContainer(Container);
+  const schema = buildSchemaSync({
+    resolvers: [`${__dirname}/**/*.resolver.ts`],
+    authChecker,
+    dateScalarMode: "timestamp"
+  });
 
-// Create the server with the routing controllers function in
-// order to feed in all our controllers
-const app = new Koa();
+  // Create the server with the routing controllers function in
+  // order to feed in all our controllers
+  const app = new Koa();
 
-// Use bodyparser for body parameters being sent
-app.use(bodyParser());
+  // Use bodyparser for body parameters being sent
+  app.use(bodyParser());
 
-// Use DI for controllers
-routingUseContainer(Container);
-useKoaServer(app, {
-  controllers: [`${__dirname}/modules/**/*.controller.ts`]
-});
+  // Use DI for controllers
+  routingUseContainer(Container);
+  useKoaServer(app, {
+    controllers: [`${__dirname}/modules/**/*.controller.ts`]
+  });
 
-// Use DI for TypeORM
-ormUseContainer(Container);
+  // Use DI for TypeORM
+  ormUseContainer(Container);
 
-// Global exception handler
-app.use(async (ctx: Context, next: any) => {
-  try {
-    await next();
-  } catch (err) {
-    console.log("Caught error:");
-    console.log(err);
-    ctx.throw(err.message, err.status);
-  }
-});
-
-// Enable the async local storage for the application
-als.enable();
-
-// Set the unique identifier for each request
-app.use(async (ctx: Context, next: any) => {
-  const requestId = ctx.request.headers["x-request-id"] || uuid();
-  als.set("requestId", requestId.replace("-", "").slice(0, 12));
-
-  await next();
-});
-
-// Setup JWT authentication for everything
-// Also set the logged in user in async local storage
-app.use(jwt({ secret: APP_KEY, passthrough: true }));
-app.use(async (ctx: Context, next: any) => {
-  if (ctx.state.user) {
-    als.set("user", ctx.state.user.email);
-  }
-
-  await next();
-});
-
-// Static files
-app.use(serve(path.join(__dirname, "..", "public")));
-
-// Morgan logger for requests
-app.use(morgan("short", { stream: _logger }));
-
-// Apollo server
-const server = new ApolloServer({
-  schema,
-  context: async ({ ctx }: { ctx: Context & { user: User } }) => {
-    if (ctx.state.user) {
-      const user = await userService.findOneById(ctx.state.user.id);
-      return { ctx, user };
+  // Global exception handler
+  app.use(async (ctx: Context, next: any) => {
+    try {
+      await next();
+    } catch (err) {
+      console.log("Caught error:");
+      console.log(err);
+      ctx.throw(err.message, err.status);
     }
-    return { ctx, user: null };
-  },
-  playground: isDevEnv()
-});
+  });
 
-// Apply GraphQL middleware to the express app
-server.applyMiddleware({ app, path: GRAPHQL_PATH });
+  // Enable the async local storage for the application
+  als.enable();
 
-// export const appListen = app.listen({ port: PORT });
-export default app;
+  // Set the unique identifier for each request
+  app.use(async (ctx: Context, next: any) => {
+    const requestId = ctx.request.headers["x-request-id"] || uuid();
+    als.set("requestId", requestId.replace("-", "").slice(0, 12));
+
+    await next();
+  });
+
+  // Setup JWT authentication for everything
+  // Also set the logged in user in async local storage
+  app.use(jwt({ secret: APP_KEY, passthrough: true }));
+  app.use(async (ctx: Context, next: any) => {
+    if (ctx.state.user) {
+      als.set("user", ctx.state.user.email);
+    }
+
+    await next();
+  });
+
+  // Static files
+  app.use(serve(path.join(__dirname, "..", "public")));
+
+  // Morgan logger for requests
+  app.use(morgan("short", { stream: _logger }));
+
+  // Apollo server
+  const server = new ApolloServer({
+    schema,
+    context: async ({ ctx }: { ctx: Context & { user: User } }) => {
+      if (ctx.state.user) {
+        const user = await userService.findOneById(ctx.state.user.id);
+        return { ctx, user };
+      }
+      return { ctx, user: null };
+    },
+    playground: isDevEnv()
+  });
+
+  // Apply GraphQL middleware to the express app
+  server.applyMiddleware({ app, path: GRAPHQL_PATH });
+
+  return app;
+}
