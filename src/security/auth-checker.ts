d72b02095a8f222419419f227e337e7804c2f219
Brian Schulte <bschulte@Brians-MacBook-Pro.local>
2019-04-18 16:13:22 -0400

    use container to get user service

--- src/security/auth-checker.ts
+++ src/security/auth-checker.ts
@@ -1,6 +1,9 @@
 import { AuthChecker } from "type-graphql";
-import * as userService from "../modules/user/user.service";
+import { UserService } from "../modules/user/user.service";
 import { roles as userRoles } from "../common/constants";
+import Container from "typedi";
+
+const userService = Container.get(UserService);
 
 export const authChecker: AuthChecker = async (
   { context }: { context: any },
